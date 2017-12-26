a4154c4831d97cc0b2290bb76bf5b1d36b7daa06
tia <larentis@spaziodati.eu>
2017-12-26 14:47:52 +0100

    add google analytics

--- processors/generate.py
+++ processors/generate.py
@@ -40,7 +40,8 @@ HIDDEN_GAMES = {
     463,     # magic-gathering
 }
 
-BASE_ITEM = ''.join(open('./layouts/partials/boardgame.html').readlines())
+boardgame_tmpl = '\n'.join(open('./layouts/partials/boardgame.html').readlines())
+google_analytics = '\n'.join(open('./layouts/partials/google-analytics.html').readlines())
 style_hash = hashlib.md5(open('./style.css').read().encode('utf-8')).hexdigest()
 input_data = json.loads(''.join(open('./processors/games.json').readlines()))
 
@@ -54,7 +55,7 @@ for item in input_data.get('items', []):
     if item['idBGG'] in GAMES_WITHOUT_MD:
         image = item['image'].replace('.jpg', '_t.jpg').replace('.png', '_t.png')
 
-    item_to_print.append(BASE_ITEM.format(
+    item_to_print.append(boardgame_tmpl.format(
         name=item['nomeGioco'],
         image=image,
         link='https://boardgamegeek.com/boardgame/{}/-'.format(item['idBGG']),
@@ -73,6 +74,8 @@ with open('./layouts/index.html') as base_index_tmpl, \
             output_index.write(line.replace('{{ number_of_games }}', str(len(item_to_print))))
         elif '{{ hash }}' in line:
             output_index.write(line.replace('{{ hash }}', style_hash))
+        elif '{{ google_analytics }}' in line:
+            output_index.write(line.replace('{{ google_analytics }}', google_analytics))
         elif '{{ blog_post }}' in line:
             for post in glob.glob('./layouts/blog/*'):
                 file_name = post.replace('./layouts/blog/', '')
@@ -80,6 +83,7 @@ with open('./layouts/index.html') as base_index_tmpl, \
                 output_index.write('<li><img src="../static/img/meeple.svg" /> <a href="/blog/{}">{}</a></li>'.format(file_name, title))
         else:
             output_index.write(line)
+
 
 with open('./layouts/games.html') as base_games_tmpl, \
         open('./games.html', 'w') as output_games:
@@ -92,18 +96,23 @@ with open('./layouts/games.html') as base_games_tmpl, \
                 output_games.write(item)
         elif '{{ number_of_games }}' in line:
             output_games.write(line.replace('{{ number_of_games }}', str(len(item_to_print))))
+        elif '{{ google_analytics }}' in line:
+            output_games.write(line.replace('{{ google_analytics }}', google_analytics))
         else:
             output_games.write(line)
+
 
 posts = glob.glob('./layouts/blog/*')
 for post in posts:
     post_name = post.rsplit('/', 1)[1]
 
     with open(post) as post_tmpl, \
-        open('./blog/{}'.format(post_name), 'w+') as output_post:
+            open('./blog/{}'.format(post_name), 'w+') as output_post:
 
         for line in post_tmpl:
             if '{{ hash }}' in line:
                 output_post.write(line.replace('{{ hash }}', style_hash))
+            elif '{{ google_analytics }}' in line:
+                output_post.write(line.replace('{{ google_analytics }}', google_analytics))
             else:
                 output_post.write(line)
